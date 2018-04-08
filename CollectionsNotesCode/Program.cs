0ac7f51b9b03ec2ea50b30db1dda56135a09dad1
Eric <wunderkammer123@gmail.com>
2018-04-08 03:19:34 -0400

    initial commit, initial array notes

--- CollectionsNotesCode/Program.cs
+++ CollectionsNotesCode/Program.cs
@@ -10,6 +10,17 @@ namespace CollectionsNotesCode
     {
         static void Main(string[] args)
         {
+            int[] lockCombination = new int[3];
+            Console.WriteLine(lockCombination);
+
+            int[] lockCombination1 = new int[] { 10, 5, 32 };
+            int[] lockCombination2 = new[] { 10, 5, 32 };
+            int[] lockCombination3 = { 10, 5, 32 };
+
+            int last = lockCombination3[2];
+            Console.WriteLine(last);
+
+            lockCombination3[2] = 20;
         }
     }
 }
