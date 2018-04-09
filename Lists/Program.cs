4f6d78be23771cd3187ac82c7cd27be4246be596
Eric <wunderkammer123@gmail.com>
2018-04-09 14:21:27 -0400

    Added some basic List notes.

---
+++ Lists/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                                                                        
+            //List<string> students = new List<string>();
+            //students.Add("Sue");
+            //students.Add("Bill");
+            //students.Add("Allen");
+
+            //students.Count
+            ////3
+            //students[0];
+            ////"Sue"
+            //students[students.Count - 1];
+            ////"Allen"
+            //students.Capacity
+            ////4
+            // When a list runs out of room it instantiates a new list with twice the capacity
+            //students.Add("Beth");
+            //students.Add("Mary");
+            //students.Count
+            ////5
+            //students.Capacity
+            ////8
+            //List<string> studentsTwo = new List<string>(500);
+            //students.Capacity
+            ////500
+        }
+    }
+}
