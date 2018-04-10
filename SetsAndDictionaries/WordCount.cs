d025c2c93bf0e001b8cd3c953d2d9a7fdcb4b95c
Eric <wunderkammer123@gmail.com>
2018-04-10 14:15:50 -0400

    Added WordCount class.

---
+++ SetsAndDictionaries/WordCount.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace SetsAndDictionaries
+{
+    public class LexicalAnalysis
+    {
+        public Dictionary<string, int> WordCount = new Dictionary<string, int>();
+
+        public void AddWord(string word)
+        {
+            if (WordCount.ContainsKey(word))
+
+            {
+                WordCount[word]++;
+            }
+            else
+            {
+                WordCount.Add(word, 1);
+            }
+
+        }
+    }
+}
