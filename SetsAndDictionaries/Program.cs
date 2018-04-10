003a16abbfce6a6a7a60c1f97fddff39298a3998
Eric <wunderkammer123@gmail.com>
2018-04-10 13:42:08 -0400

    Coded up Program2 to run MorseCodeTranslator.

--- SetsAndDictionaries/Program.cs
+++ SetsAndDictionaries/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SetsAndDictionaries
 {
@@ -44,6 +41,20 @@ namespace SetsAndDictionaries
         static void Main(string[] args)
         {
 
+            while (true)
+            {
+                Console.Write(": ");
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    break;
+                }
+
+                string output = MorseCodeTranslator.ToMorse(input);
+
+                Console.WriteLine(output);
+            }
         }
     }
 }
